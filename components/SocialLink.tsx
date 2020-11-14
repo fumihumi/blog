82d6fae6b139d8d8f0ab2e4abfce3b7e700733f1
fumihumi <t.fum1hum@gmail.com>
2020-11-15 02:00:13 +0900

    Fix Image alt attribute

--- components/SocialLink.tsx
+++ components/SocialLink.tsx
@@ -5,18 +5,22 @@ const files = {
   hatena: {
     filename: 'hatena.svg',
     url: 'https://fumihumi.hatenablog.com/',
+    service: 'hatenanblog'
   },
   twitter: {
     filename: 'twitter.png',
-    url: 'https://twitter.com/_fumihumi'
+    url: 'https://twitter.com/_fumihumi',
+    service: 'Twitter'
   },
   github: {
     filename: 'github.svg',
-    url: 'https://github.com/fumihumi'
+    url: 'https://github.com/fumihumi',
+    service: 'GitHub'
   },
   scrapbox: {
     filename: 'scrapbox.svg',
-    url: 'https://scrapbox.io/fumihumi/'
+    url: 'https://scrapbox.io/fumihumi/',
+    service: 'Scrapbox'
   }
 } as const
 
@@ -25,13 +29,14 @@ type Props = { type: SocialType }
 const ImageSize = 45
 
 const SocialLink: React.FC<Props> = ({ type }) => {
-  const { filename, url } = files[type]
+  const { filename, url, service } = files[type]
 
   return (
 
     <Link href={url} target="_blank">
       <Image
         src={`/socials/${filename}`}
+        alt={`fumihumi's social link for ${service}`}
         width={ImageSize}
         height={ImageSize}
         className={type}
